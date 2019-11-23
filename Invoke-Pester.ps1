84eb8bdb316527378eff8739b75e7e11571d3563
Mert Senel <32119844+MertSenel@users.noreply.github.com>
2019-11-24 00:08:30 +1100

    Dev (#1)
    * template setup
    
    * added ci env for running tests on build ci stage
    
    * pester test - after local pass
    
    * default values for pester test
    
    * removed mandatory flag for Ci pester task
    
    * syntax fix
    
    * change to AzureRM for CI use Aliases locally
    
    * attempt to fix pester not finding template file
    
    * add a new script to invoke pester test
    
    * location as paramater to avoid format conflict
    
    * convert location to function for uniformity
    
    * update managed resource group name
    
    * remove restriction on allowed env values
    
    * databricks without vnet
    
    * revert back to az module
    
    * modified test script to accept working dir param
    
    * fix order of dir change for CI
    
    * debug change
    
    * debug ci for pester test
    
    * pipeline as code, after successfull CI

---
+++ Invoke-Pester.ps1
@@ -0,0 +1,23 @@
+Param(
+  [string] $WorkingDirForCI
+)
+
+#This block should only be executed while running in CI pipeline
+if($WorkingDirForCI){
+Write-Output "Changing working directory to $WorkingDirForCI"
+Set-Location -Path $WorkingDirForCI
+Get-ChildItem #list directory objects
+}
+
+
+$parameters = @{ ResourceGroupName = "databricks-rg-ci"; `
+                 TemplateFile = ".\azuredeploy.json"; `
+                 ParametersFile = ".\azuredeploy.parameters.json";
+                 }
+$script = @{ Path = ".\pre-deploy-pester-test.ps1"; Parameters = $parameters }
+
+$timestamp = Get-Date -Format MM-dd-yyyy_HH_mm_ss
+
+
+
+Invoke-Pester -Script $script -OutputFile ".\TEST-Pester-Test-Results-$timestamp.xml" -OutputFormat "NUnitXML"
---
+++ pre-deploy-pester-test.ps1
@@ -0,0 +1,34 @@
+Param(
+  [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+  [string] [Parameter(Mandatory=$true)] $TemplateFile,
+  [string] [Parameter(Mandatory=$true)] $ParametersFile
+)
+
+Describe "DataBrick ARM Template Deployment Tests" {
+  BeforeAll {
+    $DebugPreference = "Continue"
+  }
+
+  AfterAll {
+    $DebugPreference = "SilentlyContinue"
+  }
+
+  Context "Run Mock Deployment with Test Cmdlet" {
+    $output = Test-AzResourceGroupDeployment `
+                              -Mode "Complete" `
+                              -ResourceGroupName $ResourceGroupName `
+                              -TemplateFile $TemplateFile `
+                              -TemplateParameterFile $ParametersFile `
+                              -ErrorAction Stop `
+                               5>&1
+
+    #debug output
+    $output
+
+    $result = (($output[27] -split "Body:")[1] | ConvertFrom-Json).properties
+
+    It "Should be deployed successfully" {
+      $result.provisioningState | Should -Be "Succeeded"
+    }
+  }
+}
