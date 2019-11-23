2436a4f74327463a1cc2de2de4ed294ed175d4b0
Mert Senel <mert.senel@sgtech.com.au>
2019-11-23 23:25:49 +1100

    fix order of dir change for CI

--- Invoke-Pester.ps1
+++ Invoke-Pester.ps1
@@ -2,6 +2,13 @@ Param(
   [string] $WorkingDirForCI
 )
 
+#This block should only be executed while running in CI pipeline
+if($WorkingDirForCI){
+Write-Output "Changing working directory to $WorkingDirForCI"
+Set-Location -Path $WorkingDirForCI
+}
+
+
 $parameters = @{ ResourceGroupName = "databricks-rg-ci"; `
                  TemplateFile = ".\azuredeploy.json"; `
                  ParametersFile = ".\azuredeploy.parameters.json";
@@ -10,10 +17,6 @@ $script = @{ Path = ".\pre-deploy-pester-test.ps1"; Parameters = $parameters }
 
 $timestamp = Get-Date -Format MM-dd-yyyy_HH_mm_ss
 
-if($WorkingDirForCI){
-Write-Output "Changing working directory to $WorkingDirForCI"
-Set-Location -Path $WorkingDirForCI
-}
 
 
 Invoke-Pester -Script $script -OutputFile ".\TEST-Pester-Test-Results-$timestamp.xml" -OutputFormat "NUnitXML"
