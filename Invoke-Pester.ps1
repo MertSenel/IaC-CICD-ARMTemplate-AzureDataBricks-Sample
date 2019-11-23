ad3c0b32ea7b5b49af93ffae5afa6fa622c38248
Mert Senel <mert.senel@sgtech.com.au>
2019-11-23 23:23:50 +1100

    modified test script to accept working dir param

--- Invoke-Pester.ps1
+++ Invoke-Pester.ps1
@@ -1,3 +1,7 @@
+Param(
+  [string] $WorkingDirForCI
+)
+
 $parameters = @{ ResourceGroupName = "databricks-rg-ci"; `
                  TemplateFile = ".\azuredeploy.json"; `
                  ParametersFile = ".\azuredeploy.parameters.json";
@@ -5,5 +9,11 @@ $parameters = @{ ResourceGroupName = "databricks-rg-ci"; `
 $script = @{ Path = ".\pre-deploy-pester-test.ps1"; Parameters = $parameters }
 
 $timestamp = Get-Date -Format MM-dd-yyyy_HH_mm_ss
+
+if($WorkingDirForCI){
+Write-Output "Changing working directory to $WorkingDirForCI"
+Set-Location -Path $WorkingDirForCI
+}
+
 
 Invoke-Pester -Script $script -OutputFile ".\TEST-Pester-Test-Results-$timestamp.xml" -OutputFormat "NUnitXML"
